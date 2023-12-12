a676b75d3deeeede07afebff526881fb1ae08e90
vinod <vinod.khetade@pharmarack.com>
2023-12-12 18:16:56 +0530

    Update users.controller.ts

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,12 @@
-import { Body, Controller, Delete, Get, HttpCode, HttpException, HttpStatus, Param, Post, Res, UseFilters, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpStatus, Param, Post, Res, UseFilters, UsePipes, ValidationPipe } from '@nestjs/common';
 import { Public } from 'src/auth/decorators/public.decorator';
 import { UserService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
-import { User } from './schemas/user.schema';
 import { Response } from 'express';
 import { sendResponse } from '../utils';
 import {statusMessage} from '../constant/statusMessage'
 import { HttpExceptionFilter } from 'src/utils/http-exception.filter';
-import { responseData } from 'src/interface/common';
+import { responseData, userData } from 'src/interface/common';
 
 
 @Public()
@@ -24,12 +23,12 @@ export class UserController {
   }
 
   @Get()
-  async findAll(): Promise<User[]> {
+  async findAll(): Promise<userData[]> {
     return this.userService.findAll();
   }
 
   @Get(":id")
-  async findOne(@Param("id") id: string): Promise<User> {
+  async findOne(@Param("id") id: string): Promise<userData> {
     return this.userService.findOne(id);
   }
 
