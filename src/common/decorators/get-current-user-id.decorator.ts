5d84ba6e7efff45a412f5d7b022844fe937b2f21
vinod <vinodk2757@gmail.com>
2023-12-22 00:02:07 +0530

    removed extra files

--- src/common/decorators/get-current-user-id.decorator.ts
+++
@@ -1,9 +0,0 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-
-export const GetCurrentUserId = createParamDecorator(
-  (data: undefined, context: ExecutionContext): string => {
-    const request = context.switchToHttp().getRequest();
-    console.log(request)
-    return request.user['sub'];
-  },
-);
--- src/common/decorators/index.ts
+++ src/common/decorators/index.ts
@@ -1,3 +1,1 @@
-export * from './get-current-user.decorator';
-export * from './get-current-user-id.decorator';
 export * from './cookies.decorator';
