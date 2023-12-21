c448d23b9bd533d08998d87c1c2e028d95048f41
vinod <vinodk2757@gmail.com>
2023-12-22 00:50:37 +0530

    swagger api doc

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const options = new DocumentBuilder()
+  .setTitle('Jwt authentication example')
+  .setDescription('The User API description')
+  .setVersion('1.0')
+  .addTag('users')
+  .addBearerAuth()
+  .build();
+const document = SwaggerModule.createDocument(app, options);
+SwaggerModule.setup('api', app, document);
   await app.listen(3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
