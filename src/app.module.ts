7e36a9cab64a3926e3b0aa0099dcc44c5683a295
vinod <vinodk2757@gmail.com>
2023-12-24 00:05:36 +0530

    enviroment variable added

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from  './users/users.module';
 import { AuthModule }  from './auth/auth.module'
+import { ConfigModule } from '@nestjs/config';
+console.log(`${process.cwd()}/.env.${process.env.NODE_ENV}`)
+
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/test'),
+    ConfigModule.forRoot({   
+      envFilePath: `${process.cwd()}/.env.${process.env.NODE_ENV}`,
+      isGlobal: true  
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
     UserModule,
     AuthModule
   ]
