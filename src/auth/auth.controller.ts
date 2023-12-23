0a107e12948c9b3f593105628914306df22b4fcc
vinod <vinodk2757@gmail.com>
2023-12-23 13:14:21 +0530

    refrsh token

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -8,7 +8,8 @@ import {
   UseFilters,
   UseGuards,
   Headers,
-  Req
+  Req,
+  UnauthorizedException
 } from "@nestjs/common";
 import { SignInDto } from './dto/signIn.dto'
 import { Response, Request } from "express";
@@ -16,6 +17,7 @@ import { AuthService } from "./auth.service";
 import { Public } from "./decorators/public.decorator";
 import { HttpExceptionFilter } from "../utils/http-exception.filter";
 import { AuthGuard } from "../common/guards/index";
+import { JwtService } from '@nestjs/jwt';
 import {
   sendResponse,
   loginSuccessResponse,
@@ -23,11 +25,16 @@ import {
 } from "../utils/index";
 import { statusMessage } from "../constant/statusMessage";
 import { ApiResponse, ApiTags } from "@nestjs/swagger";
+import { jwtConstants } from "./constants";
 
 @ApiTags("auth")
 @Controller("auth")
 export class AuthController {
-  constructor(private authService: AuthService) {}
+  [x: string]: any;
+  constructor(
+    private authService: AuthService,
+    private jwtService: JwtService
+  ) {}
 
   @ApiResponse(loginSuccessResponse)
   @ApiResponse(loginErrorResponse)
@@ -57,7 +64,6 @@ export class AuthController {
       secure: false,
     });
 
-
     return sendResponse(
       res,
       HttpStatus.OK,
@@ -67,7 +73,6 @@ export class AuthController {
     );
   }
 
-
   // todo not implented yet
   // @Public()
   // @UseGuards(AuthGuard)
@@ -75,8 +80,77 @@ export class AuthController {
   @Post("/refresh")
   @UseFilters(new HttpExceptionFilter())
   async refreshTokens(@Req() request: Request, @Res() res: Response) {
-    console.log('#############refreshToken',request.cookies.refresh_token)
+    const tokens = null;
+    let refreshToken = request?.cookies?.refresh_token;
+    if (!refreshToken) {
+      return res.sendStatus(403);
+    }
+
+    try {
+      const payload = await this.jwtService.verifyAsync(refreshToken, {
+        secret: jwtConstants.secret,
+      });
+
+      if (payload) {
+        delete payload.iat;
+        delete payload.exp;
+      }
+
+      const tokens = await this.getTokens(payload);
+      console.log(tokens);
+    } catch (error) {
+      console.log(error);
+      throw new UnauthorizedException();
+    }
+    res.cookie("access_token", tokens.access_token, {
+      httpOnly: false,
+      expires: new Date(Date.now() + 1 * 24 * 60 * 60 * 1000),
+      path: "/",
+      sameSite: "none",
+      secure: false,
+    });
+
+    res.cookie("refresh_token", tokens.refresh_token, {
+      httpOnly: false,
+      expires: new Date(Date.now() + 1 * 24 * 60 * 60 * 1000),
+      path: "/",
+      sameSite: "none",
+      secure: false,
+    });
     return res.sendStatus(200);
-    // return await this.authService.refreshTokens(userId, refreshToken);
+  }
+
+  async getTokens(user: any) {
+    console.log(user);
+    const [at, rt] = await Promise.all([
+      this.jwtService.signAsync(
+        {
+          sub: user.userId,
+          email: user.email,
+          username: user.username,
+        },
+        {
+          secret: jwtConstants.secret,
+          expiresIn: "24h",
+        }
+      ),
+      this.jwtService.signAsync(
+        {
+          sub: user.userId,
+          email: user.email,
+          username: user.username,
+        },
+        {
+          secret: jwtConstants.secret,
+          expiresIn: "30d",
+        }
+      ),
+    ]);
+
+    return {
+      access_token: at,
+      refresh_token: rt,
+    };
   }
 }
+
